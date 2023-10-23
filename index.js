67d211ac71556ed08c3738942078c6fb6973ef2b
Piyush <raunaksingh4099@gmail.com>
2023-10-23 19:29:09 +0530

    eleventh

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ const server = express();
 const productRouter = require('./routes/product');
 const userRouter = require('./routes/user');
 
-// Set up CORS to allow requests from your frontend URL
-// server.use(cors({
-//   origin: 'http://localhost:5173/', // Replace with the correct frontend URL
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// }));
-
-// Database connection
+// db connection
 main().catch(err => console.log(err));
 
 async function main() {
-  // Replace the MongoDB URL with your online database URL
   await mongoose.connect('mongodb+srv://sujal:mEWyNJUUc3ZgQQkF@notesync.tykgj0w.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -26,14 +18,27 @@ async function main() {
   console.log('Database connected');
 }
 
+// Schema
+
+// bodyParser
+const allowedOrigins = ['http://localhost:5173']; // Replace with your frontend origin
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 server.use(express.json());
 server.use(morgan('default'));
 server.use(express.static('public'));
 server.use('/products', productRouter.router);
 server.use('/users', userRouter.router);
 
-const PORT = process.env.PORT || 8080; // Use process.env.PORT if available
-
-server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+server.listen(5000, () => {
+  console.log('Server started');
 });
