bde2184dea3324a21331b5fa61affb861c09dcf4
Piyush <raunaksingh4099@gmail.com>
2023-10-23 14:40:43 +0530

    seventh

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/user');
 
 // Set up CORS to allow requests from your frontend URL
 server.use(cors({
-  origin: 'https://notesync-fvrk.onrender.com', // Remove the trailing slash
+  origin: 'http://localhost:5173/', // Replace with the correct frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
