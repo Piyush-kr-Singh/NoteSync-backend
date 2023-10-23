1c5a16451593d59423370fa81b62be2fc80b4f87
Piyush <raunaksingh4099@gmail.com>
2023-10-23 12:58:04 +0530

    third

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-// require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const cors=require('cors');
+const cors = require('cors');
 
 const server = express();
-const productRouter = require('./routes/product')
-const userRouter = require('./routes/user')
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
 
-// console.log('env',process.env.DB_PASSWORD)
+// Set up CORS to allow requests from your frontend URL
+server.use(cors({
+  origin: 'https://note-sync-backend-p8aaq7yaw-piyush-kr-singh.vercel.app', // Remove the trailing slash
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
-//db connection
+// Database connection
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/employees');
-  console.log('database connected')
+  // Replace the MongoDB URL with your online database URL
+  await mongoose.connect('mongodb+srv://sujal:mEWyNJUUc3ZgQQkF@notesync.tykgj0w.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('Database connected');
 }
-//Schema
 
-
-//bodyParser
-server.use(cors());
 server.use(express.json());
 server.use(morgan('default'));
 server.use(express.static('public'));
-server.use('/products',productRouter.router);
-server.use('/users',userRouter.router);
+server.use('/products', productRouter.router);
+server.use('/users', userRouter.router);
 
+const PORT = process.env.PORT || 8080; // Use process.env.PORT if available
 
-
-
-
-server.listen(8080, () => {
-  console.log('server started');
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
